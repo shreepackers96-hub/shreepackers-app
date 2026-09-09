c2f61df79ba4f48eab32282a864ab16e694b02c3
shreepackers96-hub <shreepackers96@gmail.com>
2026-09-09 17:57:12 +0530

    Deploy PWA asset: sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+﻿const CACHE_NAME = 'shree-packers-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './app.js',
+  './styles.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './favicon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE).catch(() => {});
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('script.google.com') || event.request.method !== 'GET') {
+    return;
+  }
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
